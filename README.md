24f9583326ca474130452bd9f884fe5482df3f44
Abida Choudhury <abida.choudhury@ontariotechu.net>
2022-11-19 10:47:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment2Flutter
+ Abida Choudhury 100700985
---
+++ android/app/src/main/java/com/example/assignment2flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.assignment2flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
